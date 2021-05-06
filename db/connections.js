e8372326078fa1dfe8f0da145201d6d50e1c48ad
Alfredo Tlahuel <Vfredo602@gmail.com>
2021-05-06 08:50:01 -0500

    created connection for database

---
+++ db/connections.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+const util = require('util');
+
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Your password
+  password: 'Elacg1996!',
+  database: 'employee_db',
+});
+connection.query = util.promisify(connection.query)
+
+module.exports = connection;
+
