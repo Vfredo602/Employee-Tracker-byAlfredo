0e1249ae9e3b5bea0ac56bedd27d0966c066e01a
Alfredo Tlahuel <Vfredo602@gmail.com>
2021-05-09 21:04:04 -0500

    git commit -m "index.js"

---
+++ db/index.js
@@ -0,0 +1,12 @@
+const connection = require ('./connection');
+
+class DATA{
+    constructor(connection){
+        this.connection = connection
+    }
+// //
+}
+
+
+
+module.exports = new DATA(connection)
